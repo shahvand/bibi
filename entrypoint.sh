c4bd8da80c9f58e830ebac3aaae349d7481c96c8
Jalal Shahvand <jalalshahvand@gmail.com>
2025-04-19 08:07:32 +0330

    0

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ python manage.py migrate
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 # Create superuser if specified in environment variables
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then

410ab13a4d27de5f5e9ebe44e3829e27efcc698a
Jalal Shahvand <jalalshahvand@gmail.com>
2025-05-05 10:32:49 +0330

    docker-compose build --no-cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@
 DB_HOST=${DATABASE_HOST:-192.168.100.26}
 DB_PORT=${DATABASE_PORT:-3306}
 
+# Copy important static files directly
+echo "Copying logo and favicon files..."
+cp -f $APP_HOME/static/img/logo.png $HOME/static/img/
+cp -f $APP_HOME/static/favicon.ico $HOME/static/
+cp -f $APP_HOME/static/favicon.png $HOME/static/
+
 # Wait for MySQL to be ready
 echo "Waiting for MySQL at $DB_HOST:$DB_PORT..."
 while ! nc -z $DB_HOST $DB_PORT; do
